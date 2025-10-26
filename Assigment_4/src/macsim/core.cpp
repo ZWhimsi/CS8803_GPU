b0c8e0e1d2820843228135ac5255db58154df87d
ZWhimsi <119135456+ZWhimsi@users.noreply.github.com>
2025-10-26 18:59:53 -0400

    Revert to original buffer approach - need to understand reference implementation better

--- Assigment_4/src/macsim/core.cpp
+++ Assigment_4/src/macsim/core.cpp
@@ -229,18 +229,9 @@ void core_c::run_a_cycle(){
   
   if(is_compute(trace_info->m_opcode)) {
     int latency = get_latency(trace_info->m_opcode, gpusim->tensor_latency);
+    int completion_cycle = c_cycle + latency;
     int dest_reg = trace_info->m_dst[0];
     
-    // For tensor instructions with high latency, stall the warp directly
-    if(latency > 1) {
-      stall_cycles += latency;
-      c_running_warp->trace_buffer.pop();
-      inst_count_total++;
-      return;
-    }
-    
-    // For regular compute instructions, use the execution buffer
-    int completion_cycle = c_cycle + latency;
     bool buffer_full = add_insts_to_exec_buffer(completion_cycle, c_running_warp->warp_id, dest_reg);
     if(buffer_full) {
       stall_cycles++;
